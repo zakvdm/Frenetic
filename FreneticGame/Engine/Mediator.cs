21e086d725845dc6f1e3b4b5f5bfb6d5e981ce59
Zak van der Merwe <zakvandermerwe@gmail.com>
2009-02-18 22:19:08 +0200

    Added basic Mediator.
    Not hooked up to anything yet. You can do very basic command registration, and that's about it.

---
+++ FreneticGame/Engine/Mediator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Frenetic
+{
+    public class Mediator
+    {
+        public Mediator()
+        {
+            Commands = new Dictionary<string, Func<string, string>>();
+        }
+
+        public List<string> AvailableCommands
+        {
+            get
+            {
+                return Commands.Keys.ToList();
+            }
+        }
+
+        public string Get(string propertyName)
+        {
+            if (!Commands.ContainsKey(propertyName))
+                return null;
+
+            return Commands[propertyName](null);
+        }
+        public string Do(string commandName, string argument)
+        {
+            if (!Commands.ContainsKey(commandName))
+                return null;
+
+            return Commands[commandName](argument);
+        }
+
+        public Dictionary<string, Func<string, string>> Commands { get; private set; }
+    }
+}
---
+++ UnitTestLibrary/MediatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using Frenetic;
+using System.Collections.Generic;
+
+namespace UnitTestLibrary
+{
+    [TestFixture]
+    public class MediatorTests
+    {
+        [Test]
+        public void CanGetProperties()
+        {
+            Mediator mediator = new Mediator();
+            mediator.Commands.Add("TestPropertyGet", (parameter) => "worked!");
+
+            Assert.AreEqual("worked!", mediator.Get("TestPropertyGet"));
+        }
+
+        [Test]
+        public void CanGetAListOfPossibleCommands()
+        {
+            Mediator mediator = new Mediator();
+            mediator.Commands.Add("Property1", null);
+            mediator.Commands.Add("Method1", null);
+
+            List<string> possibleCommands = mediator.AvailableCommands;
+
+            Assert.AreEqual(2, possibleCommands.Count);
+            Assert.IsTrue(possibleCommands.Exists((searchString) => searchString == "Property1"));
+            Assert.IsTrue(possibleCommands.Exists((searchString) => searchString == "Method1"));
+        }
+
+        [Test]
+        public void CanCallAMethod()
+        {
+            Mediator mediator = new Mediator();
+            mediator.Commands.Add("TestMethod", (parameter) => parameter);
+
+            Assert.AreEqual("boo", mediator.Do("TestMethod", "boo"));
+        }
+
+        [Test]
+        public void ChecksIfCommandExists()
+        {
+            Mediator mediator = new Mediator();
+
+            Assert.IsNull(mediator.Get("NonExistentProperty"));
+            Assert.IsNull(mediator.Do("NonExistentMethod", "argument1"));
+        }
+    }
+}
