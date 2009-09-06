d79fb9d0305f4bf8d0caf7283320a364e74a7ae2
Zak van der Merwe <zak.vandermerwe@gmail.com>
2009-09-06 09:38:33 +0200

    Played around with different serialization options (JSON, and protocol buffers), but eventually decided to KISS and just use manual Lidgren serialization. (Still needs to be hooked up)
    As a nice bonus, packet sizes should now be pretty close to optimal (there are still some possible nitpicky optimizations out there, but with lower returns...).
    
    Extending the Lidgren NetBuffer API through Extension methods gives nice concise and consistent use:  netbuffer.Write(msg) AND var msg = netbuffer.ReadMessage().

---
+++ FreneticGame/Network/Lidgren/LidgrenSerializer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Frenetic.Player;
+using Lidgren.Network;
+using Lidgren.Network.Xna;
+using Frenetic.Weapons;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Frenetic.Network.Lidgren
+{
+    public static class FreneticLidgrenExtensionSerialization
+    {
+        // MESSAGE:
+        public static void Write(this NetBuffer netbuffer, Message message)
+        {
+            netbuffer.Write(message.Items.Count);
+            foreach (Item item in message.Items)
+            {
+                netbuffer.Write(item);
+            }
+        }
+        public static Message ReadMessage(this NetBuffer netbuffer)
+        {
+            var msg = new Message();
+
+            int item_count = netbuffer.ReadInt32();
+            for (int i = 0; i < item_count; i++)
+            {
+                msg.Items.Add(netbuffer.ReadItem());
+            }
+
+            return msg;
+        }
+
+        // ITEM:
+        public static void Write(this NetBuffer netbuffer, Item item)
+        {
+            netbuffer.Write((ushort)item.Type);
+            netbuffer.Write(item.ClientID);
+
+            switch (item.Type)
+            {
+                case ItemType.Player:
+                    netbuffer.Write((PlayerState)item.Data);
+                    break;
+                case ItemType.PlayerSettings:
+                    netbuffer.Write((NetworkPlayerSettings)item.Data);
+                    break;
+                case ItemType.NewClient:
+                case ItemType.SuccessfulJoin:
+                case ItemType.DisconnectingClient:
+                    netbuffer.Write((int)item.Data);
+                    break;
+            }
+        }
+        public static Item ReadItem(this NetBuffer netbuffer)
+        {
+            var item = new Item();
+
+            item.Type = (ItemType)netbuffer.ReadUInt16();
+            item.ClientID = netbuffer.ReadInt32();
+
+            switch (item.Type)
+            {
+                case ItemType.Player:
+                    item.Data = netbuffer.ReadPlayerState();
+                    break;
+                case ItemType.PlayerSettings:
+                    item.Data = netbuffer.ReadPlayerSettings();
+                    break;
+                case ItemType.NewClient:
+                case ItemType.SuccessfulJoin:
+                case ItemType.DisconnectingClient:
+                    item.Data = netbuffer.ReadUInt32();
+                    break;
+            }
+
+            return item;
+        }
+
+        // PLAYERSTATE:
+        public static void Write(this NetBuffer netbuffer, PlayerState state)
+        {
+            netbuffer.Write(state.IsAlive);
+            netbuffer.Write(state.NewShots.Count);
+            foreach (Shot shot in state.NewShots)
+            {
+                netbuffer.Write(shot.StartPoint);
+                netbuffer.Write(shot.EndPoint);
+            }
+            netbuffer.Write(state.Position);
+            netbuffer.Write(state.Score.Deaths);
+            netbuffer.Write(state.Score.Kills);
+        }
+        public static PlayerState ReadPlayerState(this NetBuffer netbuffer)
+        {
+            PlayerState playerstate = new PlayerState();
+           
+            playerstate.IsAlive = netbuffer.ReadBoolean();
+            int number_of_new_shots = netbuffer.ReadInt32();
+            for (int i = 0; i < number_of_new_shots; i++)
+            {
+                Shot shot = new Shot();
+                shot.StartPoint = netbuffer.ReadVector2();
+                shot.EndPoint = netbuffer.ReadVector2();
+                playerstate.NewShots.Add(shot);
+            }
+            playerstate.Position = netbuffer.ReadVector2();
+            playerstate.Score.Deaths = netbuffer.ReadInt32();
+            playerstate.Score.Kills = netbuffer.ReadInt32();
+
+            return playerstate;
+        }
+
+        public static void Write(this NetBuffer netbuffer, IPlayerSettings settings)
+        {
+            netbuffer.Write(settings.Color.PackedValue);
+            netbuffer.Write(settings.Name);
+            netbuffer.Write((ushort)settings.Texture);
+        }
+        public static IPlayerSettings ReadPlayerSettings(this NetBuffer netbuffer)
+        {
+            IPlayerSettings settings = new NetworkPlayerSettings();
+
+            // TODO: Make IsDirty internal
+            var color = new Color() { PackedValue = netbuffer.ReadUInt32() };
+            settings.Color = color;
+            settings.Name = netbuffer.ReadString();
+            settings.Texture = (Frenetic.Graphics.PlayerTexture)netbuffer.ReadUInt16();
+
+            return settings;
+        }
+
+        public static void Write(this NetBuffer netbuffer, List<ChatMessage> chatlog)
+        {
+            netbuffer.Write(chatlog.Count);
+            foreach (ChatMessage msg in chatlog)
+            {
+                netbuffer.Write(msg.ClientName);
+                netbuffer.Write(msg.Message);
+            }
+        }
+        public static List<ChatMessage> ReadChatLog(this NetBuffer netbuffer)
+        {
+            var chatlog = new List<ChatMessage>();
+
+            int message_count = netbuffer.ReadInt32();
+            for (int i = 0; i < message_count; i++)
+            {
+                chatlog.Add(new ChatMessage() { ClientName = netbuffer.ReadString(), Message = netbuffer.ReadString() });
+            }
+
+            return chatlog;
+        }
+    }
+}
---
+++ UnitTestLibrary/NetBufferExtensionMethodsTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Frenetic.Network.Lidgren;
+using Lidgren.Network;
+using Frenetic.Player;
+using Microsoft.Xna.Framework;
+using Frenetic.Weapons;
+using Frenetic.Gameplay;
+using Frenetic.Network;
+
+namespace UnitTestLibrary
+{
+    [TestFixture]
+    public class NetBufferExtensionMethodsTests
+    {
+        NetBuffer buffer;
+        [SetUp]
+        public void SetUp()
+        {
+            buffer = new NetBuffer();
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeAMessageWithManyItems()
+        {
+            var item1 = new Item() { Type = ItemType.Player, Data = new PlayerState() };
+            var item2 = new Item() { Type = ItemType.PlayerSettings, Data = new NetworkPlayerSettings() };
+            var item3 = new Item() { Type = ItemType.ChatLog, Data = new List<Frenetic.ChatMessage>() { new Frenetic.ChatMessage() { ClientName = "1", Message = "hello" } } };
+            var msg = new Message() { Items = new List<Item>() { item1, item3, item1, item2 } };
+            buffer.Write(msg);
+
+            Console.WriteLine("Serializing Message with 2 x PlayerState, 1 x ChatMessage and PlayerSettings took " + buffer.Data.Length + " bytes");
+            var output = buffer.ReadMessage();
+
+            Assert.AreEqual(4, output.Items.Count);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeItemWithPlayerState()
+        {
+            buffer.Write(new Item() { ClientID = 20, Type = ItemType.Player, Data = new PlayerState() });
+
+            Console.WriteLine("Serializing Item with PlayerState took " + buffer.Data.Length + " bytes");
+            var output = buffer.ReadItem();
+
+            Assert.AreEqual(20, output.ClientID);
+            Assert.AreEqual(ItemType.Player, output.Type);
+            Assert.AreEqual(typeof(PlayerState), output.Data.GetType());
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeItemWithPlayerSettings()
+        {
+            buffer.Write(new Item() { ClientID = 100, Type = ItemType.PlayerSettings, Data = new NetworkPlayerSettings() });
+
+            var output = buffer.ReadItem();
+
+            Assert.AreEqual(typeof(NetworkPlayerSettings), output.Data.GetType());
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeThePlayerJoinAndDisconnectMessages()
+        {
+            buffer.Write(new Item() { Type = ItemType.NewClient, Data = 3 });
+            buffer.Write(new Item() { Type = ItemType.SuccessfulJoin, Data = 4 });
+            buffer.Write(new Item() { Type = ItemType.DisconnectingClient, Data = 500 });
+
+            Assert.AreEqual(3, buffer.ReadItem().Data);
+            Assert.AreEqual(4, buffer.ReadItem().Data);
+            Assert.AreEqual(500, buffer.ReadItem().Data);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializePlayerState()
+        {
+            buffer.Write(new PlayerState() 
+                            { 
+                                IsAlive = true, 
+                                Position = new Vector2(5, 10), 
+                                NewShots = new List<Shot>() { new Shot() { EndPoint = Vector2.UnitX, StartPoint = Vector2.UnitY }, new Shot() { EndPoint = Vector2.Zero, StartPoint = Vector2.One} }, 
+                                Score = new PlayerScore() { Deaths = 5, Kills = 1 } 
+                            }
+                        );
+
+            Console.WriteLine("Serializing PlayerState took " + buffer.Data.Length + " bytes");
+            var output = buffer.ReadPlayerState();
+
+            Assert.AreEqual(true, output.IsAlive);
+            Assert.AreEqual(new Vector2(5, 10), output.Position);
+            Assert.AreEqual(2, output.NewShots.Count);
+            Assert.AreEqual(new Shot() { EndPoint = Vector2.UnitX, StartPoint = Vector2.UnitY }, output.NewShots[0]);
+            Assert.AreEqual(new Shot() { EndPoint = Vector2.Zero, StartPoint = Vector2.One }, output.NewShots[1]);
+            Assert.AreEqual(5, output.Score.Deaths);
+            Assert.AreEqual(1, output.Score.Kills);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializePlayerSettings()
+        {
+            buffer.Write(new LocalPlayerSettings()
+                            {
+                                Color = Microsoft.Xna.Framework.Graphics.Color.DarkOliveGreen,
+                                Name = "test",
+                                Texture = Frenetic.Graphics.PlayerTexture.Blank
+                            }
+                        );
+
+            Console.WriteLine("Serializing PlayerSettings took " + buffer.Data.Length + " bytes");
+            var output = buffer.ReadPlayerSettings();
+
+            Assert.AreEqual(Microsoft.Xna.Framework.Graphics.Color.DarkOliveGreen, output.Color);
+            Assert.AreEqual("test", output.Name);
+            Assert.AreEqual(Frenetic.Graphics.PlayerTexture.Blank, output.Texture);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeAChatLog()
+        {
+            buffer.Write(new List<Frenetic.ChatMessage>() { new Frenetic.ChatMessage() { ClientName = "1", Message = "hello" }, new Frenetic.ChatMessage() { ClientName = "2", Message = "hi"} });
+
+            var output = buffer.ReadChatLog();
+
+            Assert.AreEqual(2, output.Count);
+            Assert.AreEqual("1", output[0].ClientName);
+            Assert.AreEqual("hi", output[1].Message);
+        }
+    }
+}
